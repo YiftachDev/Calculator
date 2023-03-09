ef64673af0843c8781fe4541d87b36d4025a9dd6
YiftachDev <yiftach007@gmail.com>
2023-03-09 18:53:59 +0200

    Created oparate function

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+const operate = (x, y, operator) => {
+    switch (operator) {
+        case ('+'): add(x, y); break;
+        case ('-'): subtract(x, y); break;
+        case ('*'): multiply(x, y); break;
+        case ('/'): divide(x, y); break;
+    }
+}
