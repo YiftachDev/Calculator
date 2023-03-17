fefdfec0b3dd6f455d408fcdf7b9277315354360
YiftachDev <yiftach007@gmail.com>
2023-03-17 13:57:58 +0200

    Created variables for all document elements

---
+++ index.js
@@ -0,0 +1,14 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const clear = document.querySelector("#clear-btn");
+    const equal = document.querySelector(".equal");
+    const decimal = document.querySelector(".decimal");
+    const numbers = document.querySelectorAll(".number");
+    const operators = document.querySelectorAll(".operation");
+    const previousScreen = document.querySelector(".previous");
+    const currentScreen = document.querySelector(".current");
+})
