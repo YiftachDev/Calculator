0a2494c2a75ff57a81b77beee9c2626dca18eb8b
YiftachDev <yiftach007@gmail.com>
2023-03-17 14:06:57 +0200

    Added code for handaling the oparators buttons

--- index.js
+++ index.js
@@ -16,12 +16,23 @@ document.addEventListener("DOMContentLoaded", () => {
         handleNumber(e.target.textContent);
         currentScreen.textContent = currentValue;
     }))
+
+    operators.forEach((op) => op.addEventListener("click", (e) => {
+        handleOperator(e.target.textContent);
+        previousScreen.textContent = previousValue += " " + operator;
+        currentScreen.textContent = currentValue;
+    }))
 })
 
 
 function handleNumber(num) {
     if (currentValue.length <= 5) {
         currentValue += num;
-    }
-    
+    }   
+}
+
+function handleOperator(op) {
+    operator = op;
+    previousValue = currentValue;
+    currentValue = '';
 }
